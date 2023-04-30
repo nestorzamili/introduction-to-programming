7a8c6c6856f239767f39521584adf9530f25e976
Nestor <nestorzamili@gmail.com>
2023-04-30 20:57:41 +0700

    TK4-ITP

---
+++ TK4.java
@@ -0,0 +1,15 @@
+// •	Program akan menampilkan menu pilihan berupa 
+// o	1. Random Data 
+// o	2. Simulasi Bubble Sort - Ascending
+// o	3. Simulasi Selecion Sort – Ascending 
+// o	4. Simulasi Bubble Sort – Descending 
+// o	5. Simulasi Selection Sort – Descending
+// o	6. Keluar 
+// •	Menu 1 : Random Data 
+// o	User akan diminta untuk memasukan rentang batas bawah dan batas atas untuk data yang akan di random 
+// o	Program akan menghasilkan 5 angka random 
+// •	Menu 2 : akan menampilkan simulasi Bubble Sort dari putaran 1 sampai terakhir secara ascending 
+// •	Menu 3 : akan menampilkan simulasi Selection Sort dari putaran 1 sampai terakhir secara ascending 
+// •	Menu 4 : akan menampilkan simulasi Bubble Sort dari putaran 1 sampai terakhir secara descending 
+// •	Menu 5 : akan menampilkan simulasi Selection Sort dari putaran 1 sampai terakhir secara descending 
+// •	Meu 6 : Keluar
