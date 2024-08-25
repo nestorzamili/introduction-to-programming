b0fe027e0b4abcd98768d7d3ca416767af5081a6
Nestor <nestorzamili@gmail.com>
2024-08-26 02:08:11 +0700

    chore: Refactor and optimize TP1-Program-Ramalan.py code

--- TP1-Program-Ramalan.py
+++ TP1-Program-Ramalan.py
@@ -1,43 +1,61 @@
-# Program Ramalan
-print("Selamat Datang Di Program Ramalam Cupu")
-print('+' * 40)
-print()
-
-# Masukkan detail user dan pasangannya
-print("Data Anda : ")
-print('\u2764' * 7)
-nama = input("Masukkan Nama Anda  : ")
-umur = int(input("Masukkan Umur Anda  : "))
-print()
-print("Data Pasangan Anda : ")
-print('\u2764' * 13)
-nama_pasangan = input("Masukkan Nama Pasangan Anda  : ")
-umur_pasangan = int(input("Masukkan Umur Pasangan Anda  : "))
-
-print()
-print(nama, [umur], "tahun")
-
-# Mencetak pola love
-print()
-print("   ❤❤❤     ❤❤❤")
-print(" ❤❤❤❤❤,,❤❤❤❤❤")
-print(" ❤❤❤❤❤❤❤❤❤❤❤")
-print("  ❤❤❤❤❤❤❤❤❤❤")
-print("    ❤❤❤❤❤❤❤❤")
-print("      ❤❤❤❤❤")
-print("         ❤❤")
-print()
-
-print(nama_pasangan, [umur_pasangan], "tahun")
-print()
-hasil = input("Tekan ENTER untuk melihat hasil ramalan...")
-
-# memilih angka random
+import math
+from turtle import *
 import random
-random_number = random.randint(50, 100)
-cocok = round(random_number / 1.1, 2)
-print()
-print("Kecocokan anda dengan pasangan anda adalah : ", cocok, "%")
-print()
-print()
-print("Terima Kasih karena anda telah menggunakan jasa Ramalan kami.. ^^v")
+
+def hearta(k):
+    return 16 * math.sin(k)**3
+
+def heartb(k):
+    return 13 * math.cos(k) - 5 * math.cos(2 * k) - 2 * math.cos(3 * k) - math.cos(4 * k)
+
+def draw_heart():
+    speed(0)
+    color("#f73487")
+    penup()
+    goto(0, -200)
+    pendown()
+    begin_fill()
+    for i in range(1000):
+        k = i * math.pi / 500
+        goto(hearta(k) * 20, heartb(k) * 20)
+    end_fill()
+
+def display_info(nama, umur, nama_pasangan, umur_pasangan, cocok):
+    penup()
+    goto(0, 50)
+    color('white')
+    write(f"{nama} [{umur}] tahun", align="center", font=("Arial", 16, "bold"))
+    goto(0, 20)
+    write(f"{nama_pasangan} [{umur_pasangan}] tahun", align="center", font=("Arial", 16, "bold"))
+    goto(0, -10)
+    write(f"Kecocokan: {cocok}%", align="center", font=("Arial", 16, "bold"))
+    pendown()
+
+def main():
+    print("Selamat Datang Di Program Ramalan Cupu")
+    print('+' * 40)
+
+    print("Data Anda : ")
+    print('\u2764' * 7)
+    nama = input("Masukkan Nama Anda  : ")
+    umur = int(input("Masukkan Umur Anda  : "))
+    print("Data Pasangan Anda : ")
+    print('\u2764' * 13)
+    nama_pasangan = input("Masukkan Nama Pasangan Anda  : ")
+    umur_pasangan = int(input("Masukkan Umur Pasangan Anda  : "))
+
+    cocok = round(random.randint(50, 100) / 1.1, 2)
+
+    input("Tekan ENTER untuk melihat pola love dan hasil ramalan...")
+
+    bgcolor('black')
+    penup()
+    goto(0, 0)
+    pendown()
+
+    draw_heart()
+    display_info(nama, umur, nama_pasangan, umur_pasangan, cocok)
+    done()
+
+if __name__ == "__main__":
+    main()
